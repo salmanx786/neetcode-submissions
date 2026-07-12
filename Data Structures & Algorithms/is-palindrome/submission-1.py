bdf35ccf158b72af0fb575f724a721d3837d09ef
Salman Khalid <Salmankhalid2016@gmail.com>
2026-07-12 16:17:09 +0500

    Add: is-palindrome - submission-1

---
+++ Data Structures & Algorithms/is-palindrome/submission-1.py
@@ -0,0 +1,46 @@
+class Solution:
+    def isPalindrome(self, s: str) -> bool:
+
+        i = 0
+        j = len(s) - 1
+        while i < j:
+            while i < j and not s[i].isalnum():
+                i += 1
+
+            while i < j and not s[j].isalnum():
+                j -= 1
+
+            if s[i].lower() != s[j].lower():
+                return False
+            i += 1
+            j -= 1
+
+        return True
+
+        """Pattern
+
+Two Pointers (Converging with Skipping)
+
+Recognition
+
+Use this pattern when:
+
+Compare both ends.
+Need to ignore certain characters.
+Palindrome problems.
+Validation while traversing.
+
+Key Insight
+
+Don't preprocess the string.
+
+Instead, filter on the fly.
+
+This saves memory because you never build a second string.
+
+Why It Works
+
+Each pointer only moves inward.
+
+Even though there are nested loop, no character is processed repeatedly.
+"""
